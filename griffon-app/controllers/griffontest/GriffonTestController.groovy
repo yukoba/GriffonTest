ea15c5647d4ed8d3499d5a9d2d2b49d7b75e8312
Yu Kobayashi <yukoba@accelart.jp>
2014-07-20 14:33:17 +0900

    miglayout導入

--- griffon-app/controllers/griffontest/GriffonTestController.groovy
+++ griffon-app/controllers/griffontest/GriffonTestController.groovy
@@ -4,8 +4,8 @@ import java.awt.event.ActionEvent
 
 class GriffonTestController {
     // these will be injected by Griffon
-    def model
-    def view
+    GriffonTestModel model
+    GriffonTestView view
 
     // void mvcGroupInit(Map args) {
     //    // this method is called after model and view are injected
--- griffon-app/views/griffontest/GriffonTestView.groovy
+++ griffon-app/views/griffontest/GriffonTestView.groovy
@@ -1,15 +1,16 @@
 package griffontest
 
 application(title: 'GriffonTest',
-        preferredSize: [320, 80],
         pack: true,
         locationByPlatform: true,
         iconImage: imageIcon('/griffon-icon-48x48.png').image,
         iconImages: [imageIcon('/griffon-icon-48x48.png').image,
                      imageIcon('/griffon-icon-32x32.png').image,
                      imageIcon('/griffon-icon-16x16.png').image]) {
-    gridLayout(cols: 2, rows: 1)
+    migLayout(layoutConstraints: "fill")
 
-    textField(columns: 20, text: bind { model.count })
-    button('increment', actionPerformed: controller.onInc)
+    label(text: "回数：", constraints: "left")
+    textField(columns: 20, text: bind { model.count }, constraints: "wrap", editable: false)
+
+    button('increment', actionPerformed: controller.onInc, constraints: "span 2, right")
 }
